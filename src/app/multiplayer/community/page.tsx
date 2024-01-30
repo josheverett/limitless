6101a01d6340f3f00ce3f77f30b84b1d1995a2c4
Josh Everett <josh.r.everett@gmail.com>
2024-01-30 10:45:34 -0800

    chore: add framer motion

--- src/app/multiplayer/community/page.tsx
+++ src/app/multiplayer/community/page.tsx
@@ -1,9 +1,14 @@
+'use client';
+
+import { motion } from 'framer-motion';
 import { MultiplayerLayout } from '@/components/layouts/multiplayer-layout';
 
 export default function CommunityTab() {
   return (
     <MultiplayerLayout>
-      <div>Community Tab</div>
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+        <div>Community Tab</div>
+      </motion.div>
     </MultiplayerLayout>
   );
 }
--- src/app/multiplayer/shop/page.tsx
+++ src/app/multiplayer/shop/page.tsx
@@ -1,9 +1,14 @@
+'use client';
+
+import { motion } from 'framer-motion';
 import { MultiplayerLayout } from '@/components/layouts/multiplayer-layout';
 
 export default function ShopTab() {
   return (
     <MultiplayerLayout>
-      <div>Shop Tab</div>
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+        <div>Shop Tab</div>
+      </motion.div>
     </MultiplayerLayout>
   );
 }
