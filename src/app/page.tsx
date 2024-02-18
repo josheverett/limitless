1b6c29cf78ce49299f5bb00ce52fcac767c8b65f
Josh Everett <josh.r.everett@gmail.com>
2024-02-17 23:54:09 -0800

    wip: get some basic animation of the letters going

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { cx } from '@emotion/css';
 import { motion } from 'framer-motion';
 import { use4k } from '@/hooks/use-4k';
 
-
 type Position = [number, number]; // vh units
 
 // All width/height/x/y units are vh
@@ -12,11 +11,12 @@ type LineProps = {
   type: 'vertical' | 'horizontal' | 'diagonal';
   width?: number;
   startHeight?: number; // vertical only
-  position: Position;
-  startXY: Position;
-  endXY: Position;
-  positionAnimationOffset?: number; // ms
-  opacityAnimationOffset?: number; // ms
+  position: Position; // relative to parent
+  startXY: Position; // relative to element origin
+  endXY?: Position;
+  transformOrigin?: string;
+  animationOffset?: number; // ms
+  children?: React.ReactNode;
 };
 
 const LINE_WIDTH = 0.417;
@@ -29,14 +29,15 @@ export const Line = ({
   position,
   startXY,
   endXY,
-  positionAnimationOffset = 0,
-  opacityAnimationOffset = 0,
+  transformOrigin = 'top left',
+  animationOffset = 0,
+  children,
 }: LineProps) => {
   const css = use4k();
 
   let [posX, posY] = position;
   const [startX, startY] = startXY;
-  const [endX, endY] = endXY;
+  const [endX, endY] = endXY ? endXY : [0, 0];
 
   let width_ = width;
   let height = 0;
@@ -49,12 +50,14 @@ export const Line = ({
     case 'horizontal':
       height = LINE_WIDTH;
       break;
+    // I'm sure the adjustments below can be derived with math
+    // and not eyeballs buy yeah wtf is math lol.
     case 'diagonal':
       posX = posX + 0.09; // eyeballed
       posY = posY + 0.23; // eyeballed
       width_ = LINE_WIDTH;
       height = CAP_HEIGHT + 0.75; // eyeballed
-      rotate = -34;
+      rotate = -34; // eyeballed
       break;
   }
 
@@ -65,19 +68,31 @@ export const Line = ({
         position: absolute;
         top: ${String(posY)}vh;
         left: ${String(posX)}vh;
-        background: white;
         width: ${String(width_)}vh;
-        height: ${String(height)}vh;
+        background: white;
       `}
-      initial={{ x: startX, y: startY, rotate, transformOrigin: 'top left' }}
-      animate={{ x: endX, y: endY }}
-      transition={{ delay: positionAnimationOffset }}
+      initial={{
+        x: (startX * 100) + '%',
+        y: (startY * 100) + '%',
+        height: (startHeight || height) + 'vh',
+        opacity: 0,
+        rotate,
+        transformOrigin,
+      }}
+      animate={{
+        x: endX || 0,
+        y: endY || 0,
+        height: height + 'vh',
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+        ease: [0.33, 0.88, 0.87, 0.95],
+        delay:
+        animationOffset,
+      }}
     >
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: opacityAnimationOffset }}
-      />
+      {children}
     </motion.div>
   );
 };
@@ -99,33 +114,39 @@ export default function StartScreen() {
         height: 100vh;
       `
     )}>
+      {/* All instances of startHeight, startXY, and offsets are eyeballed. */}
       <div className={css`position: relative; width: 39.259vh; height: 8.056vh;`}>
         {/* "I" */}
-        <Line type="vertical" position={[0, 0]} startXY={[0, 0]} endXY={[0, 0]} />
+        <Line type="vertical" position={[0, 0]} startXY={[10, 0.25]} startHeight={CAP_HEIGHT/2} />
         {/* "N" */}
-        <Line type="vertical" position={[3.611, 0]} startXY={[0, 0]} endXY={[0, 0]} />
-        <Line type="vertical" position={[7.037, 0]} startXY={[0, 0]} endXY={[0, 0]} />
-        <Line type="diagonal" position={[3.611, 0]} startXY={[0, 0]} endXY={[0, 0]} />
+        <Line type="vertical" position={[3.611, 0]} startXY={[8, -0.25]} startHeight={CAP_HEIGHT/5}>
+          <Line type="diagonal" position={[0, 0]} startXY={[0, 0]} startHeight={CAP_HEIGHT/5} />
+        </Line>
+        <Line type="vertical" position={[7.037, 0]} startXY={[6, 0.25]} startHeight={CAP_HEIGHT/5} />
         {/* "F" */}
-        <Line type="vertical" position={[10.602, 0]} startXY={[0, 0]} endXY={[0, 0]} />
-        <Line type="horizontal" width={3.38} position={[10.602, 0]} startXY={[0, 0]} endXY={[0, 0]} />
-        <Line type="horizontal" width={2.824} position={[10.602, 2.407]} startXY={[0, 0]} endXY={[0, 0]} />
+        <Line type="vertical" position={[10.602, 0]} startXY={[4, -0.25]} startHeight={CAP_HEIGHT/5}>
+          <Line type="horizontal" width={3.38} position={[0, 0]} startXY={[0, 0]} />
+          <Line type="horizontal" width={2.824} position={[0, 2.407]} startXY={[0, 0]} />
+        </Line>
         {/* "I" */}
-        <Line type="vertical" position={[16.435, 0]} startXY={[0, 0]} endXY={[0, 0]} />
+        <Line type="vertical" position={[16.435, 0]} startXY={[2, 0.25]} startHeight={CAP_HEIGHT/2} />
         {/* "N" */}
-        <Line type="vertical" position={[19.954, 0]} startXY={[0, 0]} endXY={[0, 0]} />
-        <Line type="vertical" position={[23.38, 0]} startXY={[0, 0]} endXY={[0, 0]} />
-        <Line type="diagonal" position={[19.954, 0]} startXY={[0, 0]} endXY={[0, 0]} />
+        <Line type="vertical" position={[19.954, 0]} startXY={[-2, -0.25]} startHeight={CAP_HEIGHT/2}>
+          <Line type="diagonal" position={[0, 0]} startXY={[0, 0]} />
+        </Line>
+        <Line type="vertical" position={[23.38, 0]} startXY={[-4, 0.25]} startHeight={CAP_HEIGHT/2} />
         {/* "I" */}
-        <Line type="vertical" position={[26.898, 0]} startXY={[0, 0]} endXY={[0, 0]} />
+        <Line type="vertical" position={[26.898, 0]} startXY={[-6, -0.25]} startHeight={CAP_HEIGHT/3} />
         {/* "T" */}
-        <Line type="vertical" position={[31.528, 0]} startXY={[0, 0]} endXY={[0, 0]} />
-        <Line type="horizontal" width={3.472} position={[30, 0]} startXY={[0, 0]} endXY={[0, 0]} />
+        <Line type="vertical" position={[31.528, 0]} startXY={[-8, 0.25]} startHeight={CAP_HEIGHT/5}>
+          <Line type="horizontal" width={3.472} position={[-1.528, 0]} startXY={[0, 0]} transformOrigin="top center" />
+        </Line>
         {/* "E" */}
-        <Line type="vertical" position={[36.065, 0]} startXY={[0, 0]} endXY={[0, 0]} />
-        <Line type="horizontal" width={3.241} position={[36.065, 0]} startXY={[0, 0]} endXY={[0, 0]} />
-        <Line type="horizontal" width={2.824} position={[36.065, 2.407]} startXY={[0, 0]} endXY={[0, 0]} />
-        <Line type="horizontal" width={3.241} position={[36.065, 4.769]} startXY={[0, 0]} endXY={[0, 0]} />
+        <Line type="vertical" position={[36.065, 0]} startXY={[-10, -0.25]} startHeight={CAP_HEIGHT * 0.667}>
+          <Line type="horizontal" width={3.241} position={[0, 0]} startXY={[0, 0]} />
+          <Line type="horizontal" width={2.824} position={[0, 2.407]} startXY={[0, 0]} />
+          <Line type="horizontal" width={3.241} position={[0, 4.769]} startXY={[0, 0]} />
+        </Line>
       </div>
     </main>
   );
